40e9e01932b7d4f216dccbc01457afc310818552
Brandy gayton <99420955+Brandylee24@users.noreply.github.com>
2023-11-03 01:18:49 -0400

    Initialize dependabot/docker/docker/frontend/node-16.19.1-buster-slim

---
+++ backend/src/tasks/lightning/stats-updater.service.ts
@@ -0,0 +1,34 @@
+import logger from '../../logger';
+import lightningApi from '../../api/lightning/lightning-api-factory';
+import LightningStatsImporter from './sync-tasks/stats-importer';
+import config from '../../config';
+import { Common } from '../../api/common';
+
+class LightningStatsUpdater {
+  public async $startService(): Promise<void> {
+    logger.info(`Starting Lightning Stats service`, logger.tags.ln);
+
+    await this.$runTasks();
+    LightningStatsImporter.$run();
+  }
+
+  private async $runTasks(): Promise<void> {
+    await this.$logStatsDaily();
+
+    setTimeout(() => { this.$runTasks(); }, 1000 * config.LIGHTNING.STATS_REFRESH_INTERVAL);
+  }
+
+  /**
+   * Update the latest entry for each node every config.LIGHTNING.STATS_REFRESH_INTERVAL seconds
+   */
+  private async $logStatsDaily(): Promise<void> {
+    const date = new Date();
+    Common.setDateMidnight(date);
+    const networkGraph = await lightningApi.$getNetworkGraph();
+    await LightningStatsImporter.computeNetworkStats(date.getTime() / 1000, networkGraph);
+    
+    logger.debug(`Updated latest network stats`, logger.tags.ln);
+  }
+}
+
+export default new LightningStatsUpdater();
