b1a5996e387403eda14d7d9fc10d533389a026a1
Brandy gayton <99420955+Brandylee24@users.noreply.github.com>
2023-11-03 01:19:53 -0400

    Initialize patch-1

---
+++ frontend/src/app/app.constants.ts
@@ -0,0 +1,158 @@
+export const mempoolFeeColors = [
+  '557d00',
+  '5d7d01',
+  '637d02',
+  '6d7d04',
+  '757d05',
+  '7d7d06',
+  '867d08',
+  '8c7d09',
+  '957d0b',
+  '9b7d0c',
+  'a67d0e',
+  'aa7d0f',
+  'b27d10',
+  'bb7d11',
+  'bf7d12',
+  'bf7815',
+  'bf7319',
+  'be6c1e',
+  'be6820',
+  'bd6125',
+  'bd5c28',
+  'bc552d',
+  'bc4f30',
+  'bc4a34',
+  'bb4339',
+  'bb3d3c',
+  'bb373f',
+  'ba3243',
+  'b92b48',
+  'b9254b',
+];
+
+export const chartColors = [
+  "#D81B60",
+  "#8E24AA",
+  "#5E35B1",
+  "#3949AB",
+  "#1E88E5",
+  "#039BE5",
+  "#00ACC1",
+  "#00897B",
+  "#43A047",
+  "#7CB342",
+  "#C0CA33",
+  "#FDD835",
+  "#FFB300",
+  "#FB8C00",
+  "#F4511E",
+  "#6D4C41",
+  "#757575",
+  "#546E7A",
+  "#b71c1c",
+  "#880E4F",
+  "#4A148C",
+  "#311B92",
+  "#1A237E",
+  "#0D47A1",
+  "#01579B",
+  "#006064",
+  "#004D40",
+  "#1B5E20",
+  "#33691E",
+  "#827717",
+  "#F57F17",
+  "#FF6F00",
+  "#E65100",
+  "#BF360C",
+  "#3E2723",
+  "#212121",
+  "#263238",
+];
+
+export const poolsColor = {
+   'foundryusa': '#D81B60',
+   'antpool': '#8E24AA',
+   'f2pool': '#5E35B1',
+   'poolin': '#3949AB',
+   'binancepool': '#1E88E5',
+   'viabtc': '#039BE5',
+   'btccom': '#00897B',
+   'braiinspool': '#00ACC1',
+   'sbicrypto': '#43A047',
+   'marapool': '#7CB342',
+   'luxor': '#C0CA33',
+   'unknown': '#FDD835',
+   'okkong': '#FFB300',
+}
+
+ export const feeLevels = [1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 125, 150, 175, 200,
+  250, 300, 350, 400, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1600, 1800, 2000];
+
+export interface Language {
+  code: string;
+  name: string;
+}
+
+export const languages: Language[] = [
+   { code: 'ar', name: 'العربية' },         // Arabic
+// { code: 'bg', name: 'Български' },       // Bulgarian
+// { code: 'bs', name: 'Bosanski' },        // Bosnian
+   { code: 'ca', name: 'Català' },          // Catalan
+   { code: 'cs', name: 'Čeština' },         // Czech
+// { code: 'da', name: 'Dansk' },           // Danish
+   { code: 'de', name: 'Deutsch' },         // German
+// { code: 'et', name: 'Eesti' },           // Estonian
+// { code: 'el', name: 'Ελληνικά' },        // Greek
+   { code: 'en', name: 'English' },         // English
+   { code: 'es', name: 'Español' },         // Spanish
+// { code: 'eo', name: 'Esperanto' },       // Esperanto
+// { code: 'eu', name: 'Euskara' },         // Basque
+   { code: 'fa', name: 'فارسی' },           // Persian
+   { code: 'fr', name: 'Français' },        // French
+// { code: 'gl', name: 'Galego' },          // Galician
+   { code: 'ko', name: '한국어' },          // Korean
+// { code: 'hr', name: 'Hrvatski' },        // Croatian
+// { code: 'id', name: 'Bahasa Indonesia' },// Indonesian
+   { code: 'hi', name: 'हिन्दी' },             // Hindi
+   { code: 'it', name: 'Italiano' },        // Italian
+   { code: 'he', name: 'עברית' },           // Hebrew
+   { code: 'ka', name: 'ქართული' },         // Georgian
+// { code: 'lv', name: 'Latviešu' },        // Latvian
+   { code: 'lt', name: 'Lietuvių' },        // Lithuanian
+   { code: 'hu', name: 'Magyar' },          // Hungarian
+   { code: 'mk', name: 'Македонски' },      // Macedonian
+// { code: 'ms', name: 'Bahasa Melayu' },   // Malay
+   { code: 'nl', name: 'Nederlands' },      // Dutch
+   { code: 'ja', name: '日本語' },          // Japanese
+   { code: 'nb', name: 'Norsk' },           // Norwegian Bokmål
+// { code: 'nn', name: 'Norsk Nynorsk' },   // Norwegian Nynorsk
+   { code: 'pl', name: 'Polski' },          // Polish
+   { code: 'pt', name: 'Português' },       // Portuguese
+// { code: 'pt-BR', name: 'Português (Brazil)' }, // Portuguese (Brazil)
+   { code: 'ro', name: 'Română' },          // Romanian
+   { code: 'ru', name: 'Русский' },         // Russian
+// { code: 'sk', name: 'Slovenčina' },      // Slovak
+   { code: 'sl', name: 'Slovenščina' },     // Slovenian
+// { code: 'sr', name: 'Српски / srpski' }, // Serbian
+// { code: 'sh', name: 'Srpskohrvatski / српскохрватски' },// Serbo-Croatian
+   { code: 'fi', name: 'Suomi' },           // Finnish
+   { code: 'sv', name: 'Svenska' },         // Swedish
+   { code: 'th', name: 'ไทย' },             // Thai
+   { code: 'tr', name: 'Türkçe' },          // Turkish
+   { code: 'uk', name: 'Українська' },      // Ukrainian
+   { code: 'vi', name: 'Tiếng Việt' },      // Vietnamese
+   { code: 'zh', name: '中文' },            // Chinese
+];
+
+export const specialBlocks = {
+  '709632': {
+    labelEvent: 'Taproot 🌱 activation',
+    labelEventCompleted: 'Taproot 🌱 has been activated!',
+  },
+  '840000': {
+    labelEvent: 'Halving 🥳',
+    labelEventCompleted: 'Block Subsidy has halved to 3.125 BTC per block',
+  }
+};
