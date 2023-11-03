991fee2b6750ef40bb8d38d5693d51e3090d93d6
Brandy gayton <99420955+Brandylee24@users.noreply.github.com>
2023-11-03 01:19:25 -0400

    Initialize mononaut/liquid-tooltip-fees

---
+++ frontend/src/app/lightning/nodes-ranking/top-nodes-per-channels/top-nodes-per-channels.component.ts
@@ -0,0 +1,53 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { INodesRanking, ITopNodesPerChannels } from '../../../interfaces/node-api.interface';
+import { isMobile } from '../../../shared/common.utils';
+import { GeolocationData } from '../../../shared/components/geolocation/geolocation.component';
+import { LightningApiService } from '../../lightning-api.service';
+
+@Component({
+  selector: 'app-top-nodes-per-channels',
+  templateUrl: './top-nodes-per-channels.component.html',
+  styleUrls: ['./top-nodes-per-channels.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TopNodesPerChannels implements OnInit {
+  @Input() nodes$: Observable<INodesRanking>;
+  @Input() widget: boolean = false;
+  
+  topNodesPerChannels$: Observable<ITopNodesPerChannels[]>;
+  skeletonRows: number[] = [];
+
+  constructor(
+    private apiService: LightningApiService,
+  ) {}
+
+  ngOnInit(): void {
+    for (let i = 1; i <= (this.widget ? (isMobile() ? 8 : 7) : 100); ++i) {
+      this.skeletonRows.push(i);
+    }
+
+    if (this.widget === false) {
+      this.topNodesPerChannels$ = this.apiService.getTopNodesByChannels$().pipe(
+        map((ranking) => {
+          for (const i in ranking) {
+            ranking[i].geolocation = <GeolocationData>{
+              country: ranking[i].country?.en,
+              city: ranking[i].city?.en,
+              subdivision: ranking[i].subdivision?.en,
+              iso: ranking[i].iso_code,
+            };
+          }
+          return ranking;
+        })
+      );
+    } else {
+      this.topNodesPerChannels$ = this.nodes$.pipe(
+        map((ranking) => {
+          return ranking.topByChannels.slice(0, isMobile() ? 8 : 7);
+        })
+      );
+    }
+  }
+
+}
